9b36c984703a060fb7e744241f553c4516be0c34
adilku <adhan2a@gmail.com>
2021-10-08 01:43:30 +0300

    clean up

--- Build/Release/Src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/adilkhankussidenov/Documents/PathPlanning/git_rstar/Rstar_path_planning/Src
-# Build directory: /Users/adilkhankussidenov/Documents/PathPlanning/git_rstar/Rstar_path_planning/Build/Release/Src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
